6abcd65a590b262e73a2ee216617299d65ce0417
lee jungeun <jungeun623@gmail.com>
2024-07-29 18:05:38 +0900

    반응형

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// 상단의 마이페이지 리스트 기능
 
-// swiper ================
+// 로고
+
+// swiper 배너================
 // 프로모션 스와이퍼 기능
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+// 상품영역
+
+// 브랜드 소개 배너
+
+//멤버십
+
+// 하단
