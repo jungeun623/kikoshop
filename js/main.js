a9103fa348252a1fe3e310e3d79a2440da70079f
lee jungeun <jungeun623@gmail.com>
2024-08-05 17:05:23 +0900

    gotop 생성

--- js/main.js
+++ js/main.js
@@ -35,4 +35,16 @@ const swBanner = new Swiper(".sw-banner", {
 
 // 하단
 
+// gotop
+  AOS.init();
+  // gotop바로가기 기능
+  const gotop = this.document.querySelector(".gotop");
+  //   위로 가기 기능
+  gotop.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
 })
